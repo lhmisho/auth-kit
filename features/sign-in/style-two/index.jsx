85e93c355ed7d73105f67d082a35f23e23fc84f7
lhmisho <lhmisho@gmail.com>
2020-01-19 00:47:41 +0600

    connect redux with login-form two

--- features/sign-in/style-two/index.jsx
+++ features/sign-in/style-two/index.jsx
@@ -1,31 +1,30 @@
 import React, {useState} from 'react'
 import { Formik } from 'formik'
 import Form from '../../../components/SignIn'
-import * as yup from 'yup';
-
-const validationSchema = yup.object().shape({
-    email: yup.string().email().required(),
-    password: yup.string().min(6).required()
-})
-
+import { loginTwoValidate } from '../../../utils/validation'
+import { useDispatch, useSelector, connect} from 'react-redux'
+import {login} from '../../../store/auth-reducer'
 const LoginForm = () => {
     const [email, setEmail] = useState('')
     const [password, setPassword] = useState('')
+    const dispatch = useDispatch();
+    const user = useSelector(state => state.user)
     return (
         <React.Fragment>
             <Formik
                 component={Form}
                 initialValues={{email, password}}
-                validationSchema={validationSchema}
+                validationSchema={loginTwoValidate}
                 validateOnBlur={true}
                 validateOnChange={false}
                 onSubmit={(values, formikBag) => {
-                    if(values.email !== 'lhmisho@gmail.com'){
-                        formikBag.setErrors({email: 'wrong email bitch'})
-                    }else{
-                        console.log(values)
-                        formikBag.resetForm()
-                    }
+                    // if(values.email !== 'lhmisho@gmail.com'){
+                    //     formikBag.setErrors({email: 'wrong email bitch'})
+                    // }else{
+                    //     console.log(values)
+                    //     formikBag.resetForm()
+                    // }
+                    dispatch(login(values))
                 }}
             />
         </React.Fragment>
